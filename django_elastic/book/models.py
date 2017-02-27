7cb3c953f05b5b1054ff108d5c551cc4e2e1a28c
Pavel <pavelmpd82@gmail.com>
2017-02-27 09:28:19 +0000

    Add Book model

--- django_elastic/book/models.py
+++ django_elastic/book/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(verbose_name="Title", max_length=255, null=False)
+    isbn = models.CharField(verbose_name="ISBN", max_length=50, null=True)
