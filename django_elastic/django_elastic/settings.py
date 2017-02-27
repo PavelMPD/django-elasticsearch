be23465f2d4ce0b9c32539a99b6bbdde83e117e7
Pavel <pavelmpd82@gmail.com>
2017-02-27 09:54:02 +0000

    Setup haystack

--- django_elastic/django_elastic/settings.py
+++ django_elastic/django_elastic/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'haystack',
+
     'book',
 ]
 
@@ -119,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'books',
+    },
+}
